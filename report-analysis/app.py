628877ddd326aac5dbf3d09a6d449f6a9bfe1d2e
David <dcapella@threatconnect.com>
2026-06-22 17:26:04 -0500

    Add initial structure for Basic Playbook App
    - Introduced essential files including app.py, run.py, and app_inputs.py.
    - Configured .gitignore, .coveragerc, and .pre-commit-config.yaml for project setup.
    - Created manifest.json and requirements.txt for dependency management.
    - Added app_spec.yml and install.json to define app specifications and configurations.
    - Included README.md for documentation and initial release notes.
    - Established tcex.json for template metadata and exclusions.

---
+++ report-analysis/app.py
@@ -0,0 +1,20 @@
+"""ThreatConnect Exchange Playbook App"""
+
+from playbook_app import PlaybookApp
+
+
+class App(PlaybookApp):
+    """ThreatConnect Exchange App"""
+
+    def run(self):
+        """Run the App main logic.
+
+        This method should contain the core logic of the App.
+        """
+
+    def write_output(self):
+        """Write the Playbook output variables.
+
+        This method should be overridden with the output variables defined in the install.json
+        configuration file.
+        """
